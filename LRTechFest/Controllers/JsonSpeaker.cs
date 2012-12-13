555b12c9733b4c097e408dabe1e000484b11f263
Daniel Pollock <daniellpollock@gmail.com>
2012-12-12 21:27:26 -0600

    Added speakers.json and views/links to the list.
    Signed-off-by: Daniel Pollock <daniellpollock@gmail.com>

---
+++ LRTechFest/Controllers/JsonSpeaker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace LRTechFest.Controllers
+{
+    public class Speaker
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Bio { get; set; }
+        public string ShortBio { get { return Bio.Length > 150 ? Bio.Substring(0, 150) + "..." : Bio; }
+        }
+        public string Website { get; set; }
+        public string Photo { get; set; }
+        public  List<Presentation> Presentations {get;set;}
+
+        public int Id { get; set; }
+    }
+
+    public class Presentation
+    {
+        public string Track { get; set; }
+        public string Topic { get; set; }
+        public string Description { get; set; } 
+    }
+}
--- LRTechFest/Controllers/SpeakController.cs
+++ LRTechFest/Controllers/SpeakController.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 
 namespace LRTechFest.Controllers
 {
@@ -21,5 +20,24 @@ namespace LRTechFest.Controllers
             return View();
         }
 
+        public ActionResult List()
+        {
+            string speakerfile = System.IO.File.ReadAllText(Server.MapPath("~/speakers.json"));
+            var speakerObjects = JsonConvert.DeserializeObject<List<Speaker>>(speakerfile);
+
+
+
+            return View(speakerObjects);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            string speakerfile = System.IO.File.ReadAllText(Server.MapPath("~/speakers.json"));
+            var speakerObjects = JsonConvert.DeserializeObject<List<Speaker>>(speakerfile);
+
+
+            return View(speakerObjects.FirstOrDefault(x => x.Id == id));
+
+        }
     }
 }
