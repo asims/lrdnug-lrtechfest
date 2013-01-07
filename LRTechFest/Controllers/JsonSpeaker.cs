d2eb9201c788e75d644dc284645b8a9329097dfe
Abby Sims <abby@idestini.com>
2013-01-06 21:25:27 -0600

    Initial update to layout and addition of images.

--- LRTechFest/Controllers/JsonSpeaker.cs
+++ LRTechFest/Controllers/JsonSpeaker.cs
@@ -7,13 +7,16 @@ namespace LRTechFest.Controllers
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Bio { get; set; }
-        public string ShortBio { get { return Bio.Length > 150 ? Bio.Substring(0, 150) + "..." : Bio; }
+        public string ShortBio { get { return Bio.Length > 150 ? "<p>" + Bio.Substring(0, 150) + "...</p>" : "<p>" +  Bio + "</p>"; }
         }
         public string Website { get; set; }
         public string Photo { get; set; }
         public  List<Presentation> Presentations {get;set;}
 
         public int Id { get; set; }
+
+
+        public string FullName { get { return this.FirstName + " " + this.LastName; } }
     }
 
     public class Presentation
